cfb6fdf7e0668598e6049b86389d4ef8b8b2eb11
Juhun Kim <johnjhkim@hanmail.net>
2018-05-30 14:08:05 +0900

    done toggling

---
+++ CheckList/CheckListViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by Release on 2018. 5. 25..
+//  Copyright © 2018년 Juhun Kim. All rights reserved.
+//
+
+import UIKit
+
+class CheckListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "CheckListItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        }
+        else if indexPath.row % 5 == 1{
+            label.text = "Brush my teeth"
+        }
+        else if indexPath.row % 5 == 2{
+            label.text = "Lean iOS development"
+        }
+        else if indexPath.row % 5 == 3{
+            label.text = "Soccer practice"
+        }
+        else if indexPath.row % 5 == 4{
+            label.text = "Eat ice cream"
+        }
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        /* Remove Selection Mark */
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
